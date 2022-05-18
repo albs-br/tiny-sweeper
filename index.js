c350f6997f2f64ab19aeda1de062327fd3bf7695
Andre Baptista <albs_br@yahoo.com.br>
2022-05-18 18:33:22 -0300

    v.0.5.0

--- index.js
+++ index.js
@@ -2,13 +2,39 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const appDiv = document.getElementById('app');
 
+    const BOMBS_NUMBER = 99;
     const COLUMNS = 12;
     const LINES = 24;
-    const CELL_SIDE = Math.trunc(screen.width / COLUMNS); //px
-    //const CELL_SIDE = 8; //%
-    const BOMBS_NUMBER = 10;
-    
+    let cellClickedWidth;
 
+    const DrawPlayfield = () => {
+        //const CELL_SIDE = 8; //%
+        let cellSide = Math.trunc(screen.width / COLUMNS); //px
+
+        // Set width and height based on screen width
+        let width = cellSide - 6;
+        cellClickedWidth = cellSide - 1;
+
+        // Draw playfield
+        let playfieldHTML = '';
+        for (let line = 0; line < LINES; line++) {
+            for (let col = 0; col < COLUMNS; col++) {
+            playfieldHTML += `<div id='cell_${line}_${col}' class='cell unclicked' data-line='${line}' data-col='${col}' `
+                + ` style='left: ${(col * cellSide)}px; top: ${(line * cellSide)}px; width: ${width}px; height: ${width}px;' `
+                + ` onclick='window.CellClick(this);'></div>`;
+            }
+        }
+        appDiv.innerHTML = playfieldHTML;
+        
+        // Update matrix to include ref for each HTML cell
+        for (let line = 0; line < LINES; line++) {
+            for (let col = 0; col < COLUMNS; col++) {
+            matrix[line][col].cell = document.getElementById(
+                'cell_' + line + '_' + col
+            );
+            }
+        }
+    };
 
     // Initialize playfield matrix
     var matrix = [];
@@ -23,6 +49,11 @@ document.addEventListener("DOMContentLoaded", function() {
         };
       }
     }
+
+    DrawPlayfield();
+
+
+
     
     // Randomly put bombs
     for (let bomb = 0; bomb < BOMBS_NUMBER; bomb++) {
@@ -74,28 +105,8 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
 
-    // Set width and height based on screen width
-    let width = CELL_SIDE - 6;
 
-    // Draw playfield
-    let playfieldHTML = '';
-    for (let line = 0; line < LINES; line++) {
-      for (let col = 0; col < COLUMNS; col++) {
-        playfieldHTML += `<div id='cell_${line}_${col}' class='cell unclicked' data-line='${line}' data-col='${col}' `
-            + ` style='left: ${(col * CELL_SIDE)}px; top: ${(line * CELL_SIDE)}px; width: ${width}px; height: ${width}px;' `
-            + ` onclick='window.CellClick(this);'></div>`;
-      }
-    }
-    appDiv.innerHTML = playfieldHTML;
-    
-    // Update matrix to include ref for each HTML cell
-    for (let line = 0; line < LINES; line++) {
-      for (let col = 0; col < COLUMNS; col++) {
-        matrix[line][col].cell = document.getElementById(
-          'cell_' + line + '_' + col
-        );
-      }
-    }
+
     
     function getRandomInt(max) {
       return Math.floor(Math.random() * max);
@@ -111,32 +122,10 @@ document.addEventListener("DOMContentLoaded", function() {
       
       if (currentMatrixPos.hasBomb) {
         cell.innerHTML = '*';
-      } else {
-        if (currentMatrixPos.neighborhood > 0) {
-          cell.innerHTML = currentMatrixPos.neighborhood;
-          switch (currentMatrixPos.neighborhood) {
-            case 1:
-              cell.style.color = 'blue';
-              break;
-            case 2:
-              cell.style.color = 'green';
-              break;
-            case 3:
-              cell.style.color = 'red';
-              break;
-            case 4:
-              cell.style.color = 'darkblue';
-              break;
-            case 5:
-              cell.style.color = 'darkred';
-              break;
-            default:
-              cell.style.color = 'black';
-          }
-        } else {
-          // recursivelly find all empty cells connected to this one
-          EmptyCell(cell, "");
-        }
+      } 
+      else {
+            // recursivelly find all empty cells connected to this one
+            EmptyCell(cell, "");
       }
 
       SetCellClicked(cell);
@@ -144,19 +133,45 @@ document.addEventListener("DOMContentLoaded", function() {
     };
     window.CellClick = CellClick;
     
+    window.addEventListener('resize', function(event) {
+        DrawPlayfield();
+    }, true);
+
     const SetCellClicked = (cell) => {
         cell.classList.remove('unclicked');
         cell.classList.add('empty');
   
         // Set width and height based on screen width
-        let width = CELL_SIDE - 1;
-        cell.style.width = width + "px";
-        cell.style.height = width + "px";
+        cell.style.width = cellClickedWidth + "px";
+        cell.style.height = cellClickedWidth + "px";
 
         let line = parseInt(cell.getAttribute('data-line'));
         let col = parseInt(cell.getAttribute('data-col'));
         let currentMatrixPos = matrix[line][col];
         currentMatrixPos.isClicked = true;
+
+        if (currentMatrixPos.neighborhood > 0) {
+            cell.innerHTML = currentMatrixPos.neighborhood;
+            switch (currentMatrixPos.neighborhood) {
+              case 1:
+                cell.style.color = 'blue';
+                break;
+              case 2:
+                cell.style.color = 'green';
+                break;
+              case 3:
+                cell.style.color = 'red';
+                break;
+              case 4:
+                cell.style.color = 'darkblue';
+                break;
+              case 5:
+                cell.style.color = 'darkred';
+                break;
+              default:
+                cell.style.color = 'black';
+            }
+        }
     };
 
     let counter = 0;
@@ -175,13 +190,19 @@ document.addEventListener("DOMContentLoaded", function() {
             // console.info(counter + ' - ' + line + ', ' + col);
             
             let currentMatrixPos = matrix[line][col];
-            if ((currentMatrixPos.neighborhood > 0) || currentMatrixPos.hasBomb || currentMatrixPos.isClicked) return;
+            if(currentMatrixPos.hasBomb || currentMatrixPos.isClicked) {
+                return;
+            }
+            if ((currentMatrixPos.neighborhood > 0)) {
+                SetCellClicked(cell);
+                return;
+            }
             
             //cell.click();
             
             SetCellClicked(cell);
             
-            if (line > 0 && direction != "up")              EmptyCell(matrix[line - 1][col].cell, "up");
+            if (line > 0 && direction != "down")              EmptyCell(matrix[line - 1][col].cell, "up");
             if (col > 0 && direction != "right")            EmptyCell(matrix[line][col - 1].cell, "left");
             if (line < (LINES - 1) && direction != "up")    EmptyCell(matrix[line + 1][col].cell, "down");
             if (col < (COLUMNS - 1) && direction != "left") EmptyCell(matrix[line][col + 1].cell, "right");
