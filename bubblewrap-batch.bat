0bb4cb67166a4f171447566e7295acbda858028b
Andre Baptista <albs_br@yahoo.com.br>
2025-05-18 20:42:09 -0300

    v.1.1.11

--- bubblewrap-batch.bat
+++ bubblewrap-batch.bat
@@ -1,3 +1,5 @@
 REM bubblewrap init --manifest=http://albs-br.github.io/tiny-sweeper/manifest.json
 
+
+REM If this build end with error, try closing programs to free memory, it runs a VM that uses a large amount of RAM
 bubblewrap build
