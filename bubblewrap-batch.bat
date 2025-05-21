0c7b2d3198529d5b39412b137def2e25518ea8df
Andre Baptista <albs_br@yahoo.com.br>
2025-05-21 20:36:10 -0300

    v.1.1.12

--- bubblewrap-batch.bat
+++ bubblewrap-batch.bat
@@ -1,5 +1,6 @@
 REM bubblewrap init --manifest=http://albs-br.github.io/tiny-sweeper/manifest.json
 
+REM must update versionCode and versionName on each new release
 
 REM If this build end with error, try closing programs to free memory, it runs a VM that uses a large amount of RAM
 bubblewrap build
